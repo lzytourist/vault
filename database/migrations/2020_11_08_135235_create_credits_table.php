0aaadd1d7b75e34f069aad0e778260dad8751192
Monowar Hossain Khan <lzytourist@users.noreply.github.com>
2020-11-08 20:06:56 +0600

    Initial commit

---
+++ database/migrations/2020_11_08_135235_create_credits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCreditsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('credits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('from', 255);
+            $table->text('note')->nullable();
+            $table->unsignedInteger('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('credits');
+    }
+}
